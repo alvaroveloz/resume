f4a2ff74f6ac96144d5bea9db05d4c9ee5bacb65
Alvaro Veloz <alvaro.veloz@gmail.com>
2022-04-11 19:58:37 -0500

    RESUME Added 404 page

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../src/components/Layout'
+
+const custom404 = () => {
+  return (
+      <Layout>
+          <div className='p-5'>This page does not exist.</div>
+      </Layout>
+  )
+}
+
+export default custom404
